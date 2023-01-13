dfe4fa00d74f52db2d5392213be138ecdaca5d72
kallookoo <kallookoo@users.noreply.github.com>
2023-01-13 14:34:13 +0100

    Clean code

--- make-stubs.php
+++ make-stubs.php
@@ -1,22 +1,27 @@
 <?php
+/**
+ * Make Stubs
+ *
+ * @package vscode-wp-stubs
+ */
 
-$files = [
-	__DIR__ . '/vendor/php-stubs/woocommerce-stubs/woocommerce-packages-stubs.php',
-	__DIR__ . '/vendor/php-stubs/woocommerce-stubs/woocommerce-stubs.php',
-	__DIR__ . '/vendor/php-stubs/wordpress-globals/wordpress-globals.php',
-	__DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php',
-	__DIR__ . '/vendor/php-stubs/wp-cli-stubs/wp-cli-commands-stubs.php',
-	__DIR__ . '/vendor/php-stubs/wp-cli-stubs/wp-cli-i18n-stubs.php',
-	__DIR__ . '/vendor/php-stubs/wp-cli-stubs/wp-cli-stubs.php',
+$directory = __DIR__ . '/stubs';
+$packages  = [
+	'woocommerce-packages-stubs' => 'php-stubs/woocommerce-stubs',
+	'woocommerce-stubs'          => 'php-stubs/woocommerce-stubs',
+	'wordpress-globals'          => 'php-stubs/wordpress-globals',
+	'wordpress-stubs'            => 'php-stubs/wordpress-stubs',
+	'wp-cli-commands-stubs'      => 'php-stubs/wp-cli-stubs',
+	'wp-cli-i18n-stubs'          => 'php-stubs/wp-cli-stubs',
+	'wp-cli-stubs'               => 'php-stubs/wp-cli-stubs',
 ];
-
-$directory = str_replace( '\\', '/', __DIR__ ) . '/stubs';
 
 if ( ! is_dir( $directory ) ) {
 	mkdir( $directory );
 }
-foreach ( $files as $filename ) {
-	if ( file_exists( $filename ) ) {
-		copy( $filename, $directory . '/' . basename( $filename ) );
+foreach ( $packages as $basename => $package_dir ) {
+	$filename = __DIR__ . "/vendor/{$package_dir}/{$basename}.php";
+	if ( is_readable( $filename ) ) {
+		copy( $filename, "{$directory}/{$basename}.php" );
 	}
 }
